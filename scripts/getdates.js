9cccc98db07b67d9a2843b140572dc369eb54513
marcel Musuyu <marcelmusuyu9@gmail.com>
2024-09-19 12:23:56 +0200

    Final update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,7 +4,7 @@ const today = new Date();
 
 last.innerHTML = `Last modification ${new Intl.DateTimeFormat("en-US", {
   dateStyle: "full",
-}).format(today)}`;
+}).format(today)} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
 
 const year = document.querySelector("#currentyear");
 year.innerHTML = `${today.getFullYear()}`;
